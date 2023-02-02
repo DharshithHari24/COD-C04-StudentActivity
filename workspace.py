b5bbc2c86795e17e5480be811b8af7324fa89e4f
DharshithHari24 <96918607+DharshithHari24@users.noreply.github.com>
2023-02-02 19:54:10 +0530

    Create workspace.py

---
+++ workspace.py
@@ -0,0 +1,6 @@
+# Condition : a equal to 27 and b equals to 62. if a is greater then 62, print you 27-27-27 If not print 62-62-62
+
+
+# Condition : Create a input box to enter the age. and create a condition, 
+#             that if the age is above 20 print you are an adult else print you are a growing child
+# Recall the first class for creating a input box
